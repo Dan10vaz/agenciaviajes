b7d21530c93b3b0ad1a911739a0c137c35ef6441
DanielGesto <danielcristophervazquezayala@gmail.com>
2022-03-15 18:14:25 -0600

    Creamos los routes y la primer views

---
+++ routes/index.js
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const router = express.Router();
+
+router.get('/', (req, res) =>{
+    res.send('Inicio');
+});
+
+router.get('/nosotros', (req, res) =>{
+    res.render('nosotros');
+});
+
+export default router;
